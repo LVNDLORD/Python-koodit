c38d2fe73d54b72ef0d745bc94d905b2696d019f
Andrey <andydeshko@gmail.com>
2022-09-21 11:16:34 +0300

    ex 8.2 done

--- ex8.py
+++ ex8.py
@@ -15,29 +15,22 @@ connection = mariadb.connect(
 # stored in the ident column of the airport table.
 
 
-def airport_name_and_location(icao):
-    sql = "SELECT name, municipality FROM airport WHERE airport.ident = '" + icao + "'"
-
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    print(result)
-    return
-
-
-user_input = input("Enter ICAO code of the airport: ").upper()
-airport_name_and_location(user_input)
+# def airport_name_and_location(icao):
+#     sql = "SELECT name, municipality FROM airport WHERE airport.ident = '" + icao + "'"
+#
+#     cursor = connection.cursor()
+#     cursor.execute(sql)
+#     result = cursor.fetchall()
+#     print(result)
+#     return
+#
+#
+# user_input = input("Enter ICAO code of the airport: ").upper()
+# airport_name_and_location(user_input)
 
 
 # 2.Write a program that asks the user to enter the area code (for example FI) and prints out the airports located in
 # that country ordered by airport type. For example, Finland has 65 small airports, 15 helicopter airports and so on.
-
-#   returning type and count
-
-#   SELECT airport.type, count(*) from airport
-#   where airport.iso_country = 'FI' group by type order by type desc;
-
-# working but with iso_country only, not with country name
 
 def airport_types(iso):
     sql_a_type = "SELECT type, count(*) from airport "
@@ -45,48 +38,28 @@ def airport_types(iso):
     cursor = connection.cursor()
     cursor.execute(sql_a_type)
     result = cursor.fetchall()
-    print(result)
-    return
+    return result
+
+
+def country_name(iso):
+    sql_c_name = "SELECT DISTINCT country.name FROM airport, country "
+    sql_c_name += "WHERE country.iso_country = airport.iso_country AND airport.iso_country = '" + iso + "';"
+    cursor = connection.cursor()
+    cursor.execute(sql_c_name)
+    c_name = cursor.fetchall()
+    return c_name
 
 
 area_code_input = input("Enter area code: ").upper()
-airport_types(area_code_input)
+iso_country_name = country_name(area_code_input)
+values = airport_types(area_code_input)
 
-# values = [('small_airport', 65), ('medium_airport', 30), ('large_airport', 1), ('heliport', 15), ('closed', 6)] # - [list(tuples)]
-# values_a_types = airport_types()
-# print(f"values a types {values_a_types}")
-
-###################
-
-#   returning country name, type and count
-#   SELECT distinct country.name, airport.type, count(*) from airport, country
-#   where country.iso_country = airport.iso_country and airport.iso_country = 'FI'
-#   group by type order by type desc;
-###
-# def airport_types(iso):
-#     sql_a_type = "SELECT type, count(*) from airport "
-#     sql_a_type += "WHERE iso_country = '" + iso + "' GROUP BY type ORDER BY type DESC;"
-#     sql_country_name = "SELECT DISTINCT country.name FROM country, airport "
-#     sql_country_name += "WHERE country.iso_country = airport.iso_country AND airport.iso_country = '" + iso + "';"
-#     cursor = connection.cursor()
-#     cursor.execute(sql_a_type, sql_country_name)
-#     result = cursor.fetchall()
-#     print(result)
-#     print(type(result))
-#     return
-#
-# area_code_input = input("Enter area code: ").upper()
-# airport_types(area_code_input)
-###
-
-
-
-
-
-
-# SELECT distinct country.name, airport.type, count(*) from airport, country
-# where country.iso_country = airport.iso_country and airport.iso_country = 'FI'
-# group by type order by type desc;
+for tup in iso_country_name:  # Printing country name, instead of iso_code
+    print(f"{tup[0]} has: ")
+for ind, tuple_ in enumerate(values):  # Printing amount of each airport type
+    air_type = tuple_[0]
+    air_amount = tuple_[1]
+    print(f"- {str(air_amount)} {air_type}")
 
 
 # 3.
@@ -98,8 +71,6 @@ airport_types(area_code_input)
 # selecting View / Tool Windows / Python Packages in your PyCharm IDE, write geopy into the search field and finish the
 # installation.
 
-input()
-input()
 
 # fetch coordinates (x, y) - (x, y)
 # pip install geopy
